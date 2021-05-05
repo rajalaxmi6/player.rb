9b315c31849529d1eca70512b38ddcbce2e1c337
Rajalaxmi Katkam <83289230+rajalaxmi6@users.noreply.github.com>
2021-05-05 18:27:14 +0530

    Add files via upload

---
+++ Player3.rb
@@ -0,0 +1,41 @@
+class Player
+   
+   attr_accessor
+  def initialize(name)
+    @name = name
+    
+    @player1.Player.new("Dhoni")
+    @player2.Player.new("Sachin")
+  end
+   
+  def add_score(run)
+      
+        @player1.add_run(4)
+        @player1.add_run(6)
+        @player1.add_run(1)
+        @player2.add_run(6)
+     	@player2.add_run(1)
+    end
+    
+    def total_score
+       
+        @total_score = total_score
+         puts @player1.total_score
+    	puts @player2.total_score
+        
+        
+    end
+    
+   def boundaries
+       @player2.boundaries
+      
+       
+   end
+   
+   def details
+       @player1.details
+       @player2.details
+     end
+ end
+        
+    	
